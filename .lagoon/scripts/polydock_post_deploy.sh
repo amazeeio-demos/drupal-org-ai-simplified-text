230d5270443a10aabb47bb3107ba36a91bc1fb92
Dan Lemon <daniel.lemon@amazeelabs.com>
2025-10-10 22:22:47 +0200

    fix

--- .lagoon/scripts/polydock_post_deploy.sh
+++ .lagoon/scripts/polydock_post_deploy.sh
@@ -14,21 +14,21 @@ POLYDOCK_APP_IMAGE_DB_FILENAME="/app/web/sites/default/files/polydock/db-image"
 mkdir -p $POLYDOCK_TMP
 
 if [ -z "POLYDOCK_APP_IMAGE_URL" ]; then
-	export POLYDOCK_APP_IMAGE_URL=$APP_IMAGE_URL_DEFAULT
+    export POLYDOCK_APP_IMAGE_URL=$APP_IMAGE_URL_DEFAULT
 fi;
 
 if [ ! -f "$LOCKFILE" ]; then
-  echo "This is the first time the script is running"
-  cd $POLYDOCK_TMP
-  wget -O $POLYDOCK_APP_IMAGE_FILENAME -P $POLYDOCK_TMP $APP_IMAGE_URL_DEFAULT
+    echo "This is the first time the script is running"
+    cd $POLYDOCK_TMP
+    wget -O $POLYDOCK_APP_IMAGE_FILENAME -P $POLYDOCK_TMP $APP_IMAGE_URL_DEFAULT
 
-  echo "Extracing app image"
-  tar -zxf $POLYDOCK_APP_IMAGE_FILENAME
+    echo "Extracing app image"
+    tar -zxf $POLYDOCK_APP_IMAGE_FILENAME
 
-  echo "Syncing app image files"
-  rsync -a files /app/web/sites/default
+    echo "Syncing app image files"
+    rsync -a files /app/web/sites/default
 
-  cd /app
+    cd /app
 
     if [ -f "$POLYDOCK_APP_IMAGE_DB_FILENAME" ]; then
         echo "Removing collation from DB ..."
@@ -41,29 +41,30 @@ if [ ! -f "$LOCKFILE" ]; then
         echo "There is no database image at: $POLYDOCK_APP_IMAGE_DB_FILENAME"
     fi;
 
-  if [ ! -z "$AI_LLM_API_TOKEN" ]; then
-    echo "Importing amazee Private AI keys"
-    drush config:set key.key.amazeeio_ai key_provider_settings.key_value $AI_LLM_API_TOKEN -y
-  fi;
+    if [ ! -z "$AI_LLM_API_TOKEN" ]; then
+        echo "Importing amazee Private AI keys"
+        drush config:set key.key.amazeeio_ai key_provider_settings.key_value $AI_LLM_API_TOKEN -y
+    fi;
 
     if [ ! -z "$AI_LLM_API_URL" ]; then
         echo "Importing amazee Private AI LLM URL"
         drush config:set ai_provider_amazeeio.settings host $AI_LLM_API_URL -y
     fi;
 
-  if [ ! -z "$POLYDOCK_GENERATED_APP_ADMIN_USERNAME" ]; then
-    echo "Updating admin username to $POLYDOCK_GENERATED_APP_ADMIN_USERNAME"
-    echo "update users_field_data set name='$POLYDOCK_GENERATED_APP_ADMIN_USERNAME' where name='admin'" | drush sql-cli
-    drush cr
+    if [ ! -z "$POLYDOCK_GENERATED_APP_ADMIN_USERNAME" ]; then
+        echo "Updating admin username to $POLYDOCK_GENERATED_APP_ADMIN_USERNAME"
+        echo "update users_field_data set name='$POLYDOCK_GENERATED_APP_ADMIN_USERNAME' where name='admin'" | drush sql-cli
+        drush cr
 
-    if [ ! -z "$POLYDOCK_GENERATED_APP_ADMIN_PASSWORD" ]; then
-      echo "Updating $POLYDOCK_GENERATED_APP_ADMIN_USERNAME password";
-      drush upwd "$POLYDOCK_GENERATED_APP_ADMIN_USERNAME" "$POLYDOCK_GENERATED_APP_ADMIN_PASSWORD"
+        if [ ! -z "$POLYDOCK_GENERATED_APP_ADMIN_PASSWORD" ]; then
+            echo "Updating $POLYDOCK_GENERATED_APP_ADMIN_USERNAME password";
+            drush upwd "$POLYDOCK_GENERATED_APP_ADMIN_USERNAME" "$POLYDOCK_GENERATED_APP_ADMIN_PASSWORD"
+        fi;
+
     fi;
-  fi;
 
-  echo "Created $LOCKFILE to ensure we don't run more than once"
-  touch $LOCKFILE
+    echo "Created $LOCKFILE to ensure we don't run more than once"
+    touch $LOCKFILE
 fi;
 
 cd /app
